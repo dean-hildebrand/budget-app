f1626176da30528105c735d6284896609ebaf1ba
Dean Hildebrand <dean.hildebrand88@gmail.com>
2026-09-02 12:59:55 +0200

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_31_123703) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_02_085236) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -21,6 +21,22 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_31_123703) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_budgets_on_user_id"
+  end
+
+  create_table "chat_messages", force: :cascade do |t|
+    t.bigint "chat_id", null: false
+    t.jsonb "content", default: [], null: false
+    t.datetime "created_at", null: false
+    t.string "role", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chat_id"], name: "index_chat_messages_on_chat_id"
+  end
+
+  create_table "chats", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_chats_on_user_id"
   end
 
   create_table "sessions", force: :cascade do |t|
@@ -54,6 +70,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_31_123703) do
   end
 
   add_foreign_key "budgets", "users"
+  add_foreign_key "chat_messages", "chats"
+  add_foreign_key "chats", "users"
   add_foreign_key "sessions", "users"
   add_foreign_key "transactions", "budgets"
 end
